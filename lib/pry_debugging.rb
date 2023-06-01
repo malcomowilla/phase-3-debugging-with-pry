055d19a1c7957f72b36ac88513b44175c78030d2
malcomowilla <owillamalcom@gmail.com>
2023-06-01 11:59:01 +0300

    solved

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,11 +1,18 @@
-
 require 'pry'
+
+
+
+
+
+
+
 # takes in a number as an argument and returns the sum of that number and 2
 def plus_two(num)
 	
-	  num = num +2
 
-
+	num = num + 2
+	#or
+	# num+=2
 	num
 end
 
